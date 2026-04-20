f2bc24000b0ed115362da2a5527742d2bd854187
AnindoChoudhury <anindo.choudhury2004@gmail.com>
2026-04-20 17:14:53 +0530

    package.json change

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -1,5 +1,3 @@
-import Signin from "./Signin"
-import Dashboard from "./Dashboard"
 import { useNavigate } from "react-router-dom"
 import { useEffect } from "react";
 export default function Root()
