b40a6db5742ce093a4351af289d3f9574b8f350d
AnindoChoudhury <anindo.choudhury2004@gmail.com>
2026-04-20 17:21:39 +0530

    Removed dotenv requirement

--- backend/db.js/connect.js
+++ backend/db.js/connect.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
-require('dotenv').config()
-const { User } =  require("../Schemas/user.model.js");
-function Connect()
-{
-mongoose.connect(`${process.env.MONGO_URL}/Paytm`).then(()=>{
-    console.log("mongo db connected")
-}).catch((err)=>
-{
-    console.log("error in connecting");
-})
-
+const { User } = require("../Schemas/user.model.js");
+function Connect() {
+  mongoose
+    .connect(`${process.env.MONGO_URL}/Paytm`)
+    .then(() => {
+      console.log("mongo db connected");
+    })
+    .catch((err) => {
+      console.log("error in connecting");
+    });
 }
 
-
-module.exports = {Connect};
+module.exports = { Connect };
